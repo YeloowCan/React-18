d5eb6b94ce0726532309ed963525e247209c9014
YeloowCan <942789838@qq.com>
2023-05-14 22:11:07 +0800

    transition

--- src/04-startTransition/index.tsx
+++ src/04-startTransition/index.tsx
@@ -1,19 +1,25 @@
-import React, { useState, useEffect, useTransition } from 'react';
+import React, { useState, useEffect, useTransition, ChangeEvent } from 'react';
 
 const App: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
   const [list, setList] = useState<any[]>([]);
   const [isPending, startTransition] = useTransition();
+
   useEffect(() => {
     // 使用了并发特性，开启并发更新
     startTransition(() => {
-      setList(new Array(10000).fill(null));
+      inputValue && setList(new Array(20000).fill(inputValue));
     });
-  }, []);
+  }, [inputValue]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <>
-      {list.map((_, i) => (
-        <div key={i}>{i}</div>
-      ))}
+      <input value={inputValue} onChange={handleChange} />
+      {isPending ? 'loading' : list.map((_, i) => <div key={i}>{_}</div>)}
     </>
   );
 };
